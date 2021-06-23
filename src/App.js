9a6c17a5ca79106d626d682bf6da22489cbeee7d
unknown <gerardomontme@gmail.com>
2021-06-23 14:34:55 -0700

    detail_modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import { Button } from 'react-bootstrap';
+import Detail from './components/Detail'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>TEST</h1>
-    </div>
-  );
+  const [modalShow, setModalShow] = useState(false);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Launch modal with grid
+        </Button>
+  
+        <Detail show={modalShow} onHide={() => setModalShow(false)} />
+      </>
+    );
 }
 
 export default App;
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,50 @@
-const Detail = () => {
+import React, { useEffect, useState } from "react";
+import { Modal, Container, Row, Col, Button } from 'react-bootstrap';
+import Checkout from './Checkout';
 
-}
+export default function Detail(props){
 
-export default Detail;
+    const [checkout, setCheckout] = useState(false);
+
+    return (
+      <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Using Grid in Modal
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+        {!checkout ? (
+          <Container>
+            <Row>
+              <Col xs={12} md={8}>
+                .col-xs-12 .col-md-8
+              </Col>
+              <Col xs={6} md={4}>
+                .col-xs-6 .col-md-4
+              </Col>
+            </Row>
+  
+            <Row>
+              <Col xs={6} md={4}>
+                .col-xs-6 .col-md-4
+              </Col>
+              <Col xs={6} md={4}>
+                .col-xs-6 .col-md-4
+              </Col>
+              <Col xs={6} md={4}>
+                .col-xs-6 .col-md-4
+              </Col>
+            </Row>
+          </Container>
+          ):(
+          <Checkout/>
+        )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
